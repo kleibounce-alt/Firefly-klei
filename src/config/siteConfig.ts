c2895dfebf68c8ec2e3d7ca355feafca6316bde3
klei <kleibounce@gmail.com>
2026-06-25 00:12:36 +0800

    随意更新

--- src/config/siteConfig.ts
+++ src/config/siteConfig.ts
@@ -6,10 +6,10 @@ const SITE_LANG = "zh_CN";
 
 export const siteConfig: SiteConfig = {
 	// 站点标题
-	title: "Firefly",
+	title: "Firefly klei",
 
 	// 站点副标题
-	subtitle: "Demo site",
+	subtitle: "love is a flower, you got to let it grow",
 
 	// 站点 URL
 	site_url: "https://firefly.cuteleaf.cn",
