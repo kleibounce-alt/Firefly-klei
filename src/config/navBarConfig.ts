3a4d9d9404ac08b2185a44d5d00f1867dd8acf25
klei <kleibounce@gmail.com>
2026-07-01 17:53:39 +0800

    chore: 将GitHub链接改为个人主页，移除QQ交流群

--- src/config/navBarConfig.ts
+++ src/config/navBarConfig.ts
@@ -79,7 +79,7 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 		children: [
 			{
 				name: "GitHub",
-				url: "https://github.com/CuteLeaf/Firefly",
+				url: "https://github.com/kleibounce-alt",
 				external: true,
 				icon: "fa7-brands:github",
 			},
@@ -88,12 +88,6 @@ const getDynamicNavBarConfig = (): NavBarConfig => {
 				url: "https://gitee.com/CuteLeaf/Firefly",
 				external: true,
 				icon: "fa7-brands:gitee",
-			},
-			{
-				name: "QQ交流群",
-				url: "https://qm.qq.com/q/ZGsFa8qX2G",
-				external: true,
-				icon: "fa7-brands:qq",
 			},
 			{
 				name: "Firefly文档",
